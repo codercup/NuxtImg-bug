9b2f28441964ef26ba759f210b7cae83fd4d5ce5
菲鸽 <1020103647@qq.com>
2024-05-21 16:13:41 +0800

    chore: comment

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <img class="w-full" src="~/assets/images/pretty-girl.png" />
+    <!-- <img class="w-full" src="~/assets/images/pretty-girl.png" /> -->
     <img class="w-full" src="/images/pretty-girl.png" />
+    <p class="text-center">下面是 NuxtImg 组件显示图片，mac正常，windows不正常！</p>
     <NuxtImg class="w-full" src="/images/pretty-girl.png" />
     <div class="container m-auto bg-yellow-100 p-4">
       <fg-content :line="40" />
