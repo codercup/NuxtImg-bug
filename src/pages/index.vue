5ccd249a778223855e06f7a34014c26f1f8e276d
菲鸽 <1020103647@qq.com>
2024-05-21 16:11:20 +0800

    feat: 使用NuxtImg

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <img class="w-full" src="~/assets/images/pretty-girl.png" />
+    <img class="w-full" src="/images/pretty-girl.png" />
+    <NuxtImg class="w-full" src="/images/pretty-girl.png" />
     <div class="container m-auto bg-yellow-100 p-4">
       <fg-content :line="40" />
     </div>
