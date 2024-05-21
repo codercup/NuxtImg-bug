cdf8ce72f321dd2cf303acb908e6d21174722beb
菲鸽 <65991901+codercup@users.noreply.github.com>
2024-05-21 16:03:16 +0800

    Initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <div class="leading-10 bg-slate-500 fixed w-full z-20 h-10">topbar</div>
+    <div class="flex-1 pt-10 bg-gray-100">
+      <slot />
+    </div>
+    <div class="h-10 leading-10 bg-slate-500">footer</div>
+  </div>
+</template>
